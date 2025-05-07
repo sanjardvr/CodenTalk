c62331b5beaf4a65e9a7fca5ec34630b46108e7f
Sanjar <lazabdufattahovsanjar10@gmail.com>
2025-05-07 05:55:58 +0500

    main class setup & start modules

--- App/Source/App.cpp
+++ App/Source/App.cpp
@@ -1,6 +1,15 @@
 #include "Core/Core.h"
+#include "../Client/client.h"
+#include "../Admin/admin.h"
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	Core::PrintHelloWorld();
+    CodenTalk app;
+    app.registerModule(new Client());
+    app.registerModule(new Admin());
+    app.run();
+    return 0;
+    // end of main
 }
--- Core/Source/Core/Core.cpp
+++ Core/Source/Core/Core.cpp
@@ -2,12 +2,16 @@
 
 #include <iostream>
 
+/*
+!!! Just an empty file, but DO NOT DELETE it. Needed for compilation
+*/
+
 namespace Core {
 
-	void PrintHelloWorld()
-	{
-		std::cout << "Hello World!\n";
-		std::cin.get();
-	}
+	// void PrintHelloWorld()
+	// {
+	// 	std::cout << "Hello World!\n";
+	// 	std::cin.get();
+	// }
 
 }
